2e4ae330e4274fc79b05933602cd064bf268e2bc
gaoxz <gaoxz@bhh.com.cn>
2018-10-31 20:24:11 +0900

    Echart imported!

--- src/main/java/cc/mrbird/gaoxz/controller/FlightRestController.java
+++ src/main/java/cc/mrbird/gaoxz/controller/FlightRestController.java
@@ -1,9 +1,12 @@
 package cc.mrbird.gaoxz.controller;
 
+import afu.org.checkerframework.checker.oigj.qual.O;
 import cc.mrbird.gaoxz.dao.FlightDao;
 import cc.mrbird.gaoxz.domain.FlightEx;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -20,9 +23,19 @@ public class FlightRestController {
 	private FlightDao flightDao;
 
 	@RequestMapping("/history")
-	public List welcome(Map<String, Object> model) {
+	public Map welcome(@RequestParam String targetDate, @RequestParam String code) {
 
-		FlightEx flights= flightDao.getTheHistoryDataOfTheFlightInTargetDate("2018-11-13", "ZH9114");
+//		String code = model.get("code").toString();
+//		String targetDate = model.get("targetDate").toString();
+
+		System.out.println(code);
+		System.out.println(targetDate);
+
+		Integer yMin = 10000;
+		Integer yMax = 0;
+		String price;
+
+		FlightEx flights= flightDao.getTheHistoryDataOfTheFlightInTargetDate(targetDate, code);
 
 		List<String> keyList = new ArrayList<String>();
 		keyList.addAll(flights.getDetail().keySet());
@@ -36,15 +49,28 @@ public class FlightRestController {
 
             List b = new ArrayList();
             b.add(key);
-            b.add(flights.getDetail().get(key));
+			price = flights.getDetail().get(key);
+            b.add(price);
+
+            if(Integer.valueOf(price) > yMax){
+				yMax = Integer.valueOf(price);
+			}
+
+			if(Integer.valueOf(price) < yMin){
+				yMin = Integer.valueOf(price);
+			}
 
             one.put("value", b);
 
             result.add(one);
 
         }
+        Map<String, Object> resultMap = new HashMap<String,Object>();
+        resultMap.put("result", result);
+		resultMap.put("yMin", yMin - 20);
+		resultMap.put("yMax", yMax + 20);
 
-		return result;
+		return resultMap;
 	}
 
 	@RequestMapping("/findAll2")
@@ -56,7 +82,7 @@ public class FlightRestController {
 
 	@RequestMapping("/findAll")
 	public Map findAll(Map<String, Object> model) {
-		List result = flightDao.getTheNewestData("2018-11-13","", "", "ZH9112");
+		List result = flightDao.getTheNewestData("2018-11-13","", "", "");
 
 		Map<String, List> resultMap = new HashMap<String,List>();
 
